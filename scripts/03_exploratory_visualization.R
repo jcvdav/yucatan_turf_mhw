8cab5b0f6e3cede09f933c6c6b8ef43fd85589e6
jcvdav <juancarlos.villader@gmail.com>
2025-07-18 11:32:57 -0400

    Added library loading, loading data, and export figures as pdfs

--- scripts/03_exploratory_visualization.R
+++ scripts/03_exploratory_visualization.R
@@ -1,9 +1,23 @@
+## SOME DESCRIPTION HERE
+
+# Load packages  ---------------------------------------------------------------
+pacman::p_load(
+  here,
+  tidyverse,
+  scales
+)
+
+# Load data --------------------------------------------------------------------
+annual_catch_by_turf <- readRDS(file = here("data/processed/annual_catch_by_turf.rds")) |> 
+  mutate(turf_name = str_remove(string = turf_name,
+                                pattern = "SCPP"))
 
 ## "Total Catch Over Time"
 
-library(scales)
 
-ggplot(annual_catch_by_turf, aes(x = year, y = catch_kg)) +
+# Visualzie the data  ----------------------------------------------------------
+## Catch over time total
+plot1 <- ggplot(annual_catch_by_turf, aes(x = year, y = catch_kg)) +
   stat_summary(fun = sum, geom = "line", color = "steelblue") +
   scale_y_continuous(labels = comma) +
   labs(
@@ -11,15 +25,14 @@ ggplot(annual_catch_by_turf, aes(x = year, y = catch_kg)) +
     y = "Catch (kg)"
   )
 
+plot1
+
 
 ## "Catch Over Time by TURF"
-
-library(tidyverse)
-
 annual_catch_by_turf_sorted <- annual_catch_by_turf %>%
   arrange(turf_name, year)
 
-ggplot(annual_catch_by_turf_sorted, aes(x = year, y = catch_kg)) +
+plot2 <- ggplot(annual_catch_by_turf_sorted, aes(x = year, y = catch_kg)) +
   geom_line(color = "blue") +
   facet_wrap(~ turf_name, scales = "free_y") +
   labs(
@@ -28,11 +41,9 @@ ggplot(annual_catch_by_turf_sorted, aes(x = year, y = catch_kg)) +
   ) +
   theme_minimal()
 
+plot2
 
 ## "Top 5 TURFs by Total Catch"
-
-library(tidyverse)
-
 ggplot(top5_turfs, aes(x = reorder(turf_name, -total_catch), y = total_catch)) +
   geom_col(fill = "lightblue") +
   labs(
@@ -40,3 +51,19 @@ ggplot(top5_turfs, aes(x = reorder(turf_name, -total_catch), y = total_catch)) +
     y = "Total Catch (kg)"
   ) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Export figures ---------------------------------------------------------------
+ggsave(plot = plot1,
+       filename = here("results/figures/total_catch_general.pdf"),
+       width = 4,
+       height = 2)
+
+ggsave(plot = plot2,
+       filename = here("results/figures/total_catch_by_TURF.pdf"),
+       width = 8,
+       height = 4)
+
+
+
+
+
